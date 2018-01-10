06787104b4187ae89ab3ed6d6aa09082b0fd2c80
kirillcheck <32092781+kirillcheck@users.noreply.github.com>
2018-01-11 00:22:18 +0300

    Update rot(test).py

--- rot(test).py
+++ rot(test).py
@@ -6,9 +6,9 @@
 def test_summ(count,expected):
 	assert(rot13(count) == expected)
 import rot13 as r
-def test_Rot13(str):
+def test_Rot13(s):
   """
   Функция проверяет функцию шифрования Rot13
   """
-  assert(r.rot13(r.rot13(str)) == str) 
-  return r.rot13(r.rot13(str)) == str
+  assert(r.rot13(r.rot13(s)) == s) 
+  return r.rot13(r.rot13(s)) == s
